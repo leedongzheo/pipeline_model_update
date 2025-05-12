fc5ae3ff01a0c43085d286158f7aaa93f74cc545
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-05-12 12:07:01 +0700

    Update Swin_unet.py

--- model/Swin_unet.py
+++ model/Swin_unet.py
@@ -781,3 +781,32 @@ class SwinTransformerSys(nn.Module):
         flops += self.num_features * self.patches_resolution[0] * self.patches_resolution[1] // (2 ** self.num_layers)
         flops += self.num_features * self.num_classes
         return flops
+class SwinUnet(nn.Module):
+    def __init__(self, img_size=256,in_chans=3, num_classes=1, zero_head=False, vis=False):
+        super(SwinUnet, self).__init__()
+        self.num_classes = num_classes
+        self.zero_head = zero_head
+        # self.config = config
+
+        self.swin_unet = SwinTransformerSys(img_size=img_size,
+                                patch_size=4,
+                                in_chans=in_chans,
+                                num_classes=self.num_classes,
+                                embed_dim=96,
+                                depths=[2, 2, 6, 2],
+                                num_heads=[3, 6, 12, 24],
+                                window_size=8,
+                                mlp_ratio=4,
+                                qkv_bias=True,
+                                qk_scale=None,
+                                drop_rate=0.0,
+                                drop_path_rate=0.1,
+                                ape=False,
+                                patch_norm=True,
+                                use_checkpoint="expand_first")
+
+    def forward(self, x):
+        if x.size()[1] == 1:
+            x = x.repeat(1,3,1,1)
+        logits = self.swin_unet(x)
+        return logits
